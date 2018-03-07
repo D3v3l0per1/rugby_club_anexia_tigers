e7af35d8d6871f18fe3c8d0cb0658f25da736d52
D3v3l0per1 <manuel.widmoser12@gmail.com>
2018-03-07 03:10:41 +0100

    Initial with Navigation

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+
+    <!-- SideNav -->
+    <v-navigation-drawer temporary fixed v-model="sideNav">
+      <v-list>
+        <v-list-tile v-for="item in menuItems" :key="item.tile">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    
+    <!-- Navbar -->
+    <v-toolbar fixed dark class="primary">
+      <v-toolbar-side-icon @click.native="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Logo</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down" v-for="item in menuItems" :key="item.title">
+        <v-btn flat>{{ item.title }}</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <router-view/>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sideNav: false,
+      menuItems: [
+        { icon: '', title: 'Home' },
+        { icon: '', title: 'News' },
+        { icon: '', title: 'Termine' },
+        { icon: '', title: 'Tigers' },
+        { icon: '', title: 'Sponsoren' },
+        { icon: '', title: 'History' },
+        { icon: '', title: 'Kontakt' }
+      ]
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#D32F2F',
+    secondary: '#E53935',
+    accent: '#D50000',
+    error: '#f44336',
+    warning: '#FFEB3B',
+    info: '#2196f3',
+    success: '#4caf50'
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
